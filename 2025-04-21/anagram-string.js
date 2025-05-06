58a4547f995363a8f8f58e9530f5b6e843716640
Rishabh0319 <rishabhtripathi0319@gmail.com>
2025-05-06 13:26:32 -0400

    empty commit

---
+++ 2025-04-21/anagram-string.js
@@ -0,0 +1,17 @@
+
+// Q -> check to see if two provided string is anagrams of eachother.
+//  one string is an anagram of another if it uses the same characters
+//  in the same quantity. only consider character, not spaces or punctuation.
+//  consider capital letters to be same as lowercase
+
+
+
+
+function anagrams(str1, str2) {
+
+}
+
+console.log(anagrams("rishabh tripathi", "tripathi rishabh"));
+console.log(anagrams("RAIL! SAFETY!", "fairy tales"));
+console.log(anagrams("hi there", "Bye there!"));
+
